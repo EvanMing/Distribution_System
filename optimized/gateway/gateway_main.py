728bc4d9c82f1dbcd6ae8fcd335986c209538dca
EvanMing <mingboost@gmail.com>
2026-03-04 16:41:54 +0800

    fix bug

--- optimized/gateway/gateway_main.py
+++ optimized/gateway/gateway_main.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     def get_gateway_host():
         import socket
         print('hostname:',socket.gethostname())
-        if "laptop" in socket.gethostname().lowwer():
+        if "laptop" in socket.gethostname().lower():
             return "127.0.0.1" 
         return '0.0.0.0'
     gateway = OptimizedGateway(gateway_host=get_gateway_host(),gateway_port=8080,server_url='http://127.0.0.1:8000')

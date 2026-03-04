05176d9c0b9b676235b63647927b87900a3d0f53
EvanMing <mingboost@gmail.com>
2026-03-04 16:33:15 +0800

    fix bug

--- optimized/server/server_main.py
+++ optimized/server/server_main.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     def get_server_host():
         import socket
         print('hostname:',socket.gethostname())
-        if "laptop" in socket.gethostname().lowwer():
+        if "laptop" in socket.gethostname().lower():
             return "127.0.0.1" 
         return '0.0.0.0'
     server = OptimizedServer(host = get_server_host(),port=8000)
